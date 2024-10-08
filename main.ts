ed49eeebf6a88a5f8c4183ff1db826b5f1872b6b
bain-liao <liao.bain@mths.ca>
2024-10-08 11:25:16 -0400

    created traffic light program

--- main.ts
+++ main.ts
@@ -1,8 +1,33 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Bain Liao
+ * Created on: Sep 2024
+ * This program simulates a traffic light
 */
 
-basic.showString('Hello, World!')
+// variables
+let neopixelStrip: neopixel.Strip = null
+
+// cleanup
+basic.clearScreen()
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.show()
+
+input.onButtonPressed(Button.A, function () {
+    //Green
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+    pause(1000)
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+    //Yellow
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
+    pause(1000)
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+    //Red
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+    pause(1000)
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.show()
+})
